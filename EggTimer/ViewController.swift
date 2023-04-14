c5bbccfa38b2ca18bb7f865737fda4d33844ac56
Anton <49029476+tosha1992@users.noreply.github.com>
2023-04-14 17:55:31 +0300

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Anton Medovnik on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
